fab71fd88a3c06bac5623a063727938c8be80d5f
xyzdevoloper <157599470+moviesenderggbot@users.noreply.github.com>
2024-09-15 02:27:03 +0530

    Update webdeploy.py

--- webdeploy.py
+++ webdeploy.py
@@ -29,3 +29,16 @@ def hello_world():
 if __name__ == "__main__":
     app.run()
   
+#Dockerfile
+
+FROM python:3.10
+
+WORKDIR /app
+
+COPY requirements.txt /app/
+
+RUN pip3 install -r requirements.txt
+
+COPY . /app
+
+CMD gunicorn app:app & python3 bot.py
